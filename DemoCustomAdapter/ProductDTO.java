63352befcb7c9626d730e0cbe7e2de317260ede2
snvn1511 <126769119+snvn1511@users.noreply.github.com>
2023-03-15 11:31:47 +0700

    Thêm hàm khởi tạo cho ProductDTO

--- DemoCustomAdapter/ProductDTO.java
+++ DemoCustomAdapter/ProductDTO.java
@@ -5,6 +5,12 @@ public class ProductDTO {
     private String name;
     private double price;
 
+    public ProductDTO(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public int getId() {
         return id;
     }
